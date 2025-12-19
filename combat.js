cf3926739c3226c165ba91b237bd1c9305fd1faa
Cassian92150 <henri.bruniau92@gmail.com>
2025-12-19 08:51:56 +0100

    Add files via upload

--- combat.js
+++ combat.js
@@ -2,24 +2,28 @@ let hercule = {
     name: "Hercule",
     attaque: 10,
     pv: 100,
+    precis:0.2,
 }
 
 let petrouchka = {
     name: "Petrouchka",
     attaque: 15,
     pv: 80,
+    precis: 0.4,
 }
+
+const perso = {hercule, petrouchka}
 
 console.log(`combat opposant ${hercule.name} possédant une attaque de ${10} et ${100} pv, à ${petrouchka.name} possédant une attaque de ${petrouchka.attaque} et ${petrouchka.pv} pv.`);
 
 function attack (){
-    if(console.log(precision()) >=0.2){
+    if(precision(hercule)){
     petrouchka.pv = petrouchka.pv - hercule.attaque
     console.log("hercule attaque petrouchka : PV de Petrouchka restant " + petrouchka.pv);
     } else {
         console.log("l'attaque a échouer");
     }
-    if(console.log(precision()) >=0.2){
+    if(precision(petrouchka)){
     hercule.pv = hercule.pv - petrouchka.attaque
     console.log("petrouchka attaque hercule : PV d'Hercule restant " + hercule.pv);
     } else {
@@ -27,17 +31,17 @@ function attack (){
     }
 }
 
-function precision() {
-  return Math.random();
-}
 
+function precision(perso) {
+  return Math.random() > perso.precis;
+}
 
 
 for (let i = 0; i < 50; i++) {
     if (hercule.pv <= 0){
         console.log("Petrouchka a gagné");
         break
-    } if (hercule.pv <= 0){
+    } else if (petrouchka.pv <= 0){
         console.log("Hercule a gagné");
         break
     } else {
